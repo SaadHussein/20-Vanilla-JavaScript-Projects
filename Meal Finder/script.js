fb6d379f9a6aa6e61ac708892f632aa0f6c10b75
SaadHussein <saadwithyara@gmail.com>
2023-03-05 18:32:47 +0200

    Upload Projects

---
+++ Meal Finder/script.js
@@ -0,0 +1,109 @@
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  random = document.getElementById("random"),
+  mealsEl = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealEl = document.getElementById("single-meal");
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  single_mealEl.innerHTML = "";
+  const term = search.value;
+
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resultHeading.innerHTML = `
+          <h2>Search results for '${term}':</h2>
+        `;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+              <div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data-mealId="${meal.idMeal}">
+                  <h3>${meal.strMeal}</h3>
+                </div>
+              </div>
+            `
+            )
+            .join("");
+        }
+      });
+
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
+function getRandomMeal() {
+  mealsEl.innerHTML = "";
+  resultHeading.innerHTML = "";
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
+submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.target.closest(".meal-info");
+  if (!mealInfo) {
+    return;
+  }
+  const mealID = mealInfo.dataset.mealid;
+  getMealById(mealID);
+});
